7195a79b86ceac2937262653f413d06a7b3d10b7
adaltonagabe <adalto.nagabe@gmail.com>
2026-03-25 20:47:52 -0300

    Adicionado exercicio5 em 25-03-2026

---
+++ Python/exercicio5.py
@@ -0,0 +1,25 @@
+nome = "adalto"
+nome = nome.capitalize()
+print(nome)
+
+nome = "adalto nagabe"
+nome2 = nome.upper()
+print(nome2)
+
+nome = "ADALTO"
+nome2 = nome.lower()
+print(nome2)
+
+nome = "ADALTO"
+nome = nome.isalnum()
+print(nome)
+
+x = '91776-6967' 
+x = x.split('-')
+print(x)
+
+x = '31295' 
+x = x.zfill(6)
+print(x)
+
+
