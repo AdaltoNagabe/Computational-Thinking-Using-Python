dd8ce15816dc8400b6246e719e748a6fef8be422
adaltonagabe <adalto.nagabe@gmail.com>
2026-04-29 20:53:38 -0300

    Funcao Lambda

---
+++ Aula-29-04-2026/Lambida.py
@@ -0,0 +1,21 @@
+def funcaoSoma(a,b):
+    return a+b
+
+
+
+print(funcaoSoma(100,200))
+
+
+#funcao lambda e uma funcao anomima. Fica escondida dentro de uma variavel
+varFuncao = lambda var: var**2
+print(varFuncao(7))
+
+
+
+#------------------------------------------------
+
+def funcQuad(argumento):
+    return argumento * 2
+
+print(funcQuad(7))
+
